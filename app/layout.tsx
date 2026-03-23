1e555f0cdfe132ffa64f62521da1b74a3dfa74d4
Jeferson <jeferson@cakto.com.br>
2026-03-23 12:32:37 -0300

    Fix build: Adicionando ToastProvider no Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,18 @@
 import './globals.css';
 import { AuthProvider } from '@/lib/authContext';
 import { ThemeProvider } from '@/components/ui/ThemeProvider';
+import { ToastProvider } from '@/components/ui/Toast'; // <- Faltava importar isso
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <body suppressHydrationWarning>
         <ThemeProvider>
-          <AuthProvider>
-            {children}
-          </AuthProvider>
+          <ToastProvider> {/* <- Faltava colocar isso em volta do App */}
+            <AuthProvider>
+              {children}
+            </AuthProvider>
+          </ToastProvider>
         </ThemeProvider>
       </body>
     </html>
